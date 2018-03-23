046f7070dcb4db1391b5c89ed26b7aa12f4e9ddd
Aaron Kim <aaron.th.kim@gmail.com>
2018-03-22 20:02:18 -0600

    Initial test case written, IO to console set up, algorithm to follow

---
+++ Trapping Rain Water/Trapping Rain Water/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trapping_Rain_Water
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProg = new Program();
+            int[] height = new int[] {0,1,0,2,1,0,1,3,2,1,2,1};
+            Console.WriteLine(string.Join(",", height) + " returns " + myProg.Trap(height));
+        }
+
+        /*
+         * Given n non-negative integers representing an elevation map 
+         * where the width of each bar is 1, compute how much water it 
+         * is able to trap after raining. 
+         * For example, Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+         */
+        public int Trap(int[] height)
+        {
+
+        }
+    }
+}
